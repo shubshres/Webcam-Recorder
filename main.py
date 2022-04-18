d925da097fcc978945eb73cd140f779585cc9d2d
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2022-04-18 12:10:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,39 @@
-import numpy as nm
+# this is the one that works
+import numpy as np
+import os
 import cv2
-from cv2 import VideoWriter_fourcc, VideoWriter
+from cv2 import VideoWriter
+from cv2 import VideoWriter_fourcc
 
-# camera video capture
-cam = cv2.VideoCapture(1)
+# open the webcam video stream
+gopro = cv2.VideoCapture(0)
 
-# save video path
-# for .avi
-out = VideoWriter('recording.avi', VideoWriter_fourcc(*'MP42'), 30.0, (1280, 720))
+# open output video file stream
+output = VideoWriter('recording.avi', VideoWriter_fourcc(*'MP42'), 15.0, (1920, 1080))
 
-# for mp4
-# out = VideoWriter('recording.mp4', VideoWriter_fourcc(*'FMP4'), 30.0, (1280, 720))
 
-# loop that will run the program
-while True:
-    # reading camera frame
-    stream_ok, frame = cam.read()
+# boolean that will run the infinite loop
+run_program = True
 
-    # if webcam is detected
+while run_program:
+    # get the frame from the webcam
+    stream_ok, frame = gopro.read()
+
     if stream_ok:
-        # show camera
-        cv2.imshow('Webcam', frame)
+        # display the current frame if the stream is ok
+        cv2.imshow('GoPro', frame)
 
-        # write frame to video file
-        out.write(frame)
+        # write frame to the video out file
+        output.write(frame)
 
-        # if user presses q, exit program
-        if cv2.waitKey(1) == ord('q'):
-            break
-    else:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-# release camera
-cam.release()
 
 # close windows
 cv2.destroyAllWindows()
 
-# release video output file stream
-out.release()
+# release gopro stream
+gopro.release()
+
+# release video output file
+output.release()
